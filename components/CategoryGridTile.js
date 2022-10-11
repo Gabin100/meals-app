1fb959ece3d9dd90fbd20210171fe465f570c48c
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2022-10-11 14:45:21 +0200

    display category list

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,13 @@
+import { Pressable, Text, View } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+  return (
+    <View>
+      <Pressable>
+        <Text>{title}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
